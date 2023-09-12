1a8476f11b29fbd81ddd687e85ad942c88305425
k-usuha <kouta528bado@yahoo.co.jp>
2023-09-12 22:34:57 +0900

    Add clipboard feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
-import { Box, Textarea, Button, VStack, Heading, Alert, AlertIcon, Container, useColorMode, IconButton, Link, HStack } from '@chakra-ui/react';
-import { FaSun, FaMoon, FaGithub } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import { Box, Textarea, Button, VStack, Heading, Alert, AlertIcon, Container, useColorMode, IconButton, Link, HStack, useToast } from '@chakra-ui/react';
+import { FaSun, FaMoon, FaGithub, FaCopy } from 'react-icons/fa';
 import MarkdownToLatexConverter from './MarkdownToLatexConverter';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const App: React.FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const [markdown, setMarkdown] = useState('');
   const [latex, setLatex] = useState('');
   const [error, setError] = useState<string | null>(null);
+  const [latexRows, setLatexRows] = useState(5); 
+  const toast = useToast();
+
+  useEffect(() => {
+    
+    const lines = latex.split('\n').length;
+    setLatexRows(lines > 5 ? lines : 5); 
+  }, [latex]);
 
   const convertToLatex = () => {
     const converter = new MarkdownToLatexConverter();
@@ -22,6 +31,30 @@ const App: React.FC = () => {
         setError("An unknown error occurred.");
       }
     }
+  };
+
+  const handleCopyToClipboard = () => {
+    
+    navigator.clipboard.writeText(latex).then(() => {
+      
+      toast({
+        title: 'Copied to Clipboard',
+        description: 'The LaTeX table has been copied to your clipboard!',
+        status: 'success',
+        duration: 2000, 
+        isClosable: true, 
+      });
+    }).catch((err) => {
+      console.error('Failed to copy to clipboard: ', err);
+      
+      toast({
+        title: 'Copy Failed',
+        description: 'Failed to copy the LaTeX table to your clipboard. Please try again.',
+        status: 'error',
+        duration: 2000, 
+        isClosable: true, 
+      });
+    });
   };
 
   return (
@@ -60,13 +93,26 @@ const App: React.FC = () => {
               {error}
             </Alert>
           )}
-          <Textarea
-            value={latex}
-            isReadOnly
-            placeholder="Your LaTeX table will appear here..."
-            variant="filled"
-            size="lg"
-          />
+          <Box position="relative">
+            <Textarea
+              value={latex}
+              isReadOnly
+              placeholder="Your LaTeX table will appear here..."
+              variant="filled"
+              size="lg"
+              minH={`${latexRows * 1.5}rem`} 
+            />
+            <Box position="absolute" top={2} right={2}>
+              <CopyToClipboard text={latex} onCopy={handleCopyToClipboard}>
+                <IconButton
+                  icon={<FaCopy />}
+                  size="sm"
+                  aria-label="Copy to Clipboard"
+                  variant="outline"
+                />
+              </CopyToClipboard>
+            </Box>
+          </Box>
         </VStack>
       </Container>
     </Box>
